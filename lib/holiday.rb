4ac0a3f90adc7398fcee34d0772b1b8788fd7c6c
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-05 01:48:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -11,7 +11,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
   holiday_hash[:winter] << supply
-  end
+  #end
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
