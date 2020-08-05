b2decf8e7c33bbcc5c2004ff28b83b78d19a6a1c
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-05 02:06:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -46,7 +46,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.each do |holiday, supply|
+  holiday_hash.collect do |season, holidays|
+    holidays.collect do |holiday, supply|
     if supply == "BBQ"
       return holiday_hash.holiday.keys.flatten
     end
