c21045d6db45613ef646edfcb3fe05381d93d79d
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-05 01:57:29 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -12,7 +12,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash[:winter].each do |holiday, supplies|
     supplies << supply
-  end 
+  end
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
