766cc2bc7e9cd03659d22a6ff00d988d17b7a7d7
Sergio Cardenas <sergiocardenas@utexas.edu>
2020-08-05 02:15:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,8 +48,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.collect do |season, holidays|
     holidays.collect do |holiday, supplies|
-      if supplies.include?("BBQ)"
-        holidays.keys.flatten
+      if supplies.include?("BBQ")
+        holiday.keys.flatten
       end
     end
   end
